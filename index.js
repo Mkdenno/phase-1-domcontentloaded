b2cb372b8388474caf9c20f8878bf9778f35cd00
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-06-13 14:50:01 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", ()=>{
+    domLoading();
+})
+function domLoading(){
+    let p= document.querySelector("#text");
+    p.innerHTML="This is really cool!";
+
+}
